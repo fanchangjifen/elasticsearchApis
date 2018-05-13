8cef0ef181ed151c4b858e928e6f5af02210b626
Gilbert Pan(潘震) <panzhen@inspur.com>
2018-05-13 16:40:22 +0800

    first commit

---
+++ src/main/java/com/gilbert/elasticsearch/GilbertApiElasticsearchApplication.java
@@ -0,0 +1,12 @@
+package com.gilbert.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GilbertApiElasticsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GilbertApiElasticsearchApplication.class, args);
+	}
+}
---
+++ src/test/java/com/gilbert/elasticsearch/GilbertApiElasticsearchApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gilbert.elasticsearch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GilbertApiElasticsearchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
